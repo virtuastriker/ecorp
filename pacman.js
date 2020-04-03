632d1f1342df260c23006fb6e314c76aa7c14ca7
Dilmano Dilbero <d.dilbero@example.org>
2020-04-03 12:59:34 +0200

    who gave us the gift of the Pipppero®

--- pacman.js
+++ pacman.js
@@ -25,7 +25,7 @@ function FindProxyForURL(url, host) {
         shExpMatch(url, "*10\.31\.*")
        )
     {
-        return "PROXY 10.30.53.191:1199";
+        return "PROXY 10.41.2.10:1199";
     }
     
 // If URL has no dots in host name, send traffic direct.
